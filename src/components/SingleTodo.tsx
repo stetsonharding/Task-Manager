cda30a89552dface1ac308f28cb2fbef8c04bb80
stetson <stetson2001@msn.com>
2024-05-25 20:00:42 -0700

    Inital isCompleted and remove task functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,33 +1,52 @@
-import React from 'react'
+import React from "react";
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 
-import "./styles.css"
+import "./styles.css";
 
-import { Todo } from './model';
+import { Todo } from "./model";
 
 type Props = {
-    todo: Todo;
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
- export const SingleTodo = ({todo, todos, setTodos}: Props) => {
+export const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleCompleteTodo = (id: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((item) =>
+        item.id === id ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+
+    console.log(todo);
+  };
+
+  const handleDeleteTask = (id: number) => {
+    const updatedTasks = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTasks);
+  };
+
   return (
-<form className="todos__single">
-  <span className="todos__single--text">{todo.todo}</span>
-  <div className="todo__actionButtons">
-    <div>
-      <PencilIcon className="action_btn" />
+    <form className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+      <div className="todo__actionButtons">
+        <div>
+          <PencilIcon className="action_btn" />
+        </div>
+        <div>
+          <TrashIcon
+            className="action_btn"
+            onClick={() => handleDeleteTask(todo.id)}
+          />
+        </div>
+        <div>
+          <PlusIcon
+            className="action_btn"
+            onClick={() => handleCompleteTodo(todo.id)}
+          />
+        </div>
       </div>
-      <div>
-      <TrashIcon className="action_btn"/>
-      </div>
-      <div>
-      <PlusIcon className="action_btn" />
-      </div>
-  </div>
-</form>
-  )
-}
-
-
+    </form>
+  );
+};
