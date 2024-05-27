8bdf48b618c06efcfff713f21ad769c6112c26d1
stetson <stetson2001@msn.com>
2024-05-27 10:44:22 -0700

    Edit, Delete, Complete. Functionality done

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 import "./styles.css";
@@ -12,6 +12,9 @@ type Props = {
 };
 
 export const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [isTaskEdited, setIsTaskEdited] = useState<boolean>(false);
+  const [editedTaskValue, setEditedTaskValue] = useState<string>(todo.todo);
+
   const handleCompleteTodo = (id: number) => {
     setTodos((prevTodos) =>
       prevTodos.map((item) =>
@@ -22,22 +25,51 @@ export const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     console.log(todo);
   };
 
-  const handleDeleteTask = (id: number) => {
+  const handleDelete = (id: number) => {
     const updatedTasks = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTasks);
   };
 
+  const handleEdit = (id: number, e:React.FormEvent<HTMLFormElement>) => {
+e.preventDefault();
+
+let updatedTodo = todos.map(item => item.id === id ? {...item, todo: editedTaskValue} : item)
+setTodos(updatedTodo)
+
+setIsTaskEdited(false)
+  };
+
   return (
-    <form className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+    <form className="todos__single" onSubmit={(e) => handleEdit(todo.id,e)}>
+      {isTaskEdited ? (
+        <input
+          type="text"
+          value={editedTaskValue}
+          onChange={(e) => setEditedTaskValue(e.target.value)}
+        />
+      ) : (
+        <span
+          className={
+            todo.isDone
+              ? "todos__single--text--completed"
+              : "todos__single--text"
+          }
+        >
+          {todo.todo}
+        </span>
+      )}
+
       <div className="todo__actionButtons">
         <div>
-          <PencilIcon className="action_btn" />
+          <PencilIcon
+            className="action_btn"
+            onClick={() => setIsTaskEdited(!isTaskEdited)}
+          />
         </div>
         <div>
           <TrashIcon
             className="action_btn"
-            onClick={() => handleDeleteTask(todo.id)}
+            onClick={() => handleDelete(todo.id)}
           />
         </div>
         <div>
